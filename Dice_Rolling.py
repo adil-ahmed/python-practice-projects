5b75ac69fe31cc672bfacb0f1672bad737e789e4
adil-ahmed <adil.aj95@gmail.com>
2024-09-15 23:35:46 -0400

    Added  two features

--- Dice_Rolling.py
+++ Dice_Rolling.py
@@ -1,15 +1,74 @@
 import random
-
+counter = 0
 while True:
     response = input("Roll the dice? (y/n): ").lower()
-    print(response)
     if response == 'y':
-        dice1 = random.randint(1, 6)
-        dice2 = random.randint(1, 6)
-        print(f'({dice1}, {dice2})')
+        counter = counter + 1
+        
+        rolls = {}  # Dictionary to store the dice rolls
+        # Initialize the number variable
+        number = 0
+
+        # Keep asking for a valid number (greater than 0)
+        while number <= 0:
+            number = int(input("How many dice you want to roll? "))
+            if number <= 0:
+                print("Insert a positive number greater than 0")
+
+        # Roll the dice and store the results
+        for i in range(1, number + 1):
+            dice1 = random.randint(1, 6)
+            dice2 = random.randint(1, 6)
+            rolls[i] = (dice1, dice2)  # Store the dice roll results in the dictionary
+
+        # Print the stored dice rolls
+        for roll_number, dice_values in rolls.items():
+            print(f'Roll {roll_number}: {dice_values}')
+    
     elif response == 'n':
-        print("No problem! Thanks for playing. Have a good day.")
+        time_word = "time" if counter == 1 else "times"
+        print(f'You rolled the dice {counter} {time_word}. Thanks for playing. Have a good day.')
         break
+        
+    
     else:
         print("Invalid choice!")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    #     number = int(input("How many dice you want to roll? "))
+    #     if number > 0:
+    #         for i in range(1, number + 1):
+    #             dice1 = random.randint(1, 6)
+    #             dice2 = random.randint(1, 6)
+    #             rolls[i] = (dice1, dice2)  # Store the dice roll results in the dictionary
+
+    #         # Print the stored dice rolls
+    #         for roll_number, dice_values in rolls.items():
+    #             print(f'Roll {roll_number}: {dice_values}')
+            
+    #     else:
+    #         print("Insert a positive number greater than 0")
+    
+    # elif response == 'n':
+    #     print(f'You roll the dice {counter} times. Thanks for playing. Have a good day.')
+    #     break
+    # else:
+    #     print("Invalid choice!")
 
